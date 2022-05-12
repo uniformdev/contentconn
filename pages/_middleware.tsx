bcc63e582d280c958e2f71a2e9461aa7ca74e48a
Uniform Demo <alex.shyba@uniform.dev>
2022-05-12 14:23:07 -0700

    hmm

--- pages/_middleware.tsx
+++ pages/_middleware.tsx
@@ -44,7 +44,7 @@ export default async function middleware(request: NextRequest) {
     headers: {
       ...response.headers,
       "Cache-Control": "no-store, must-revalidate",
-      "Content-Encoding": "gzip",
+      "Accept-Encoding": "br",
       Expires: "0",
     },
   });
